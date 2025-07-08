716505f8b0cbb26675ec67fb2ba4334336ba31fe
Wojtek Kania <wojtek@kania.sh>
2025-07-08 10:48:28 +0200

    hm: add nvim configuration

--- flake.nix
+++ flake.nix
@@ -3,39 +3,48 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     disko.url = "github:nix-community/disko";
     flake-utils.url = "github:numtide/flake-utils";
+    vim.url = "github:wk93/vim";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, disko, home-manager, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      in {
-        formatter = pkgs.nixpkgs-fmt;
-      }) // {
-        nixosConfigurations.t480 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            ./hosts/t480
-            disko.nixosModules.disko
-            inputs.home-manager.nixosModules.default
-          ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    disko,
+    home-manager,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+  in {
+    formatter = pkgs.nixpkgs-fmt;
 
-	homeConfigurations = {
-          "wojtek@t480" = home-manager.lib.homeManagerConfiguration {
-            pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            extraSpecialArgs = {inherit inputs;};
-            modules = [
-              ./home.nix
-            ];
-          };
+    nixosConfigurations.t480 = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./hosts/t480
+        disko.nixosModules.disko
+        home-manager.nixosModules.default
+      ];
+    };
+
+    homeConfigurations = {
+      "wojtek@t480" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          inherit inputs system;
         };
+        modules = [
+          ./home.nix
+        ];
       };
+    };
+  };
 }
--- home.nix
+++ home.nix
@@ -1,6 +1,10 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  inputs,
+  system,
+  ...
+}: {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "wojtek";
@@ -21,6 +25,8 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     pkgs.hello
+
+    inputs.vim.packages.${system}.default
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
