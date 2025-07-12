67ae3671aca5377413759e88a5b57bf1f53a3379
Wojciech Kania <wojtek@kania.sh>
2025-07-12 16:13:59 +0200

    firefox: add ublock, privacy badger and 1password

--- home.nix
+++ home.nix
@@ -297,6 +297,29 @@ in {
         Snippets = false;
         Locked = true;
       };
+
+      ExtensionSettings = {
+        "*".installation_mode = "blocked";
+        # uBlock Origin:
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+        # Privacy Badger:
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+          private_browsing = true;
+        };
+        # 1Password:
+        "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+          default_area = "navbar";
+          private_browsing = true;
+        };
+      };
     };
     profiles = {
       "default" = {
